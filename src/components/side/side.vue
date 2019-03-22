53fd4eb8c2a8d62bbb9f3284575b0c68d2747656
latabu <koakumaping@163.com>
2019-03-22 10:14:16 +0800

    feta: change style

--- src/components/side/side.vue
+++ src/components/side/side.vue
@@ -73,7 +73,7 @@ $menu-bg-color = #2B3654
   // box-shadow: 0 0 8px rgba(0, 0, 0, 0.12), 0 16px 16px rgba(0, 0, 0, 0.24)
   box-shadow: 2px 0 6px rgba(0, 21, 41, 0.35)
   height: 100%
-  padding-top: 50px
+  padding-top: 48px
   z-index: 5
   transition: all 0.3s
 
